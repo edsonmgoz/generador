35784222d3aa17bda54d5b0c8f57d1a6f7407acc
edsonmgoz <edsonmgoz@gmail.com>
2015-03-21 21:58:52 -0400

    Generador de datos version Beta2

--- config.php
+++ config.php
@@ -3,22 +3,12 @@ session_start();
 class Conectar
 {
 
-	// Variables de entorno de OpenShift
-	$dbhost = getenv("OPENSHIFT_MYSQL_DB_HOST");
-	$dbuser = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-
-	$dbpassword = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-	$dbname = getenv("OPENSHIFT_APP_NAME");
-
-	// Conexion OpenShift
-	// mysql_connect($dbhost, $dbuser, $dbpassword) or die(mysql_error());
-	//mysql_select_db($dbname) or die(mysql_error());
 
 
 	//Conexion a la base de datos
 	public function con($database)
 	{
-		$con = mysql_connect($dbhost, $dbuser, $dbpassword) or die ('cannot connect to the database: ' . mysql_error());
+		$con = mysql_connect("localhost", "adminDWlndT1", "WnhgutjRlRyX") or die ('cannot connect to the database: ' . mysql_error());
 		mysql_query("SET NAMES 'utf8'");
 		$db_selected = mysql_select_db($database);
 		if (!$db_selected)
